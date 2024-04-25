69a4454bc8e1aa43419bc4fbfc0754b644764285
Anna <git@anna.lgbt>
2024-04-25 12:02:52 -0400

    feat: support specifying elevations

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{ops::Range, path::PathBuf};
 
 use serde::{de::{value::StrDeserializer, Visitor}, Deserialize};
 
@@ -31,14 +31,22 @@ pub struct Wallpaper {
 }
 
 pub enum During {
-    Single(Light),
-    Multiple(Vec<Light>),
+    Lights(Vec<Light>),
+    Elevation {
+        rising: Vec<Range<f64>>,
+        setting: Vec<Range<f64>>,
+    },
+    LightsAndElevation {
+        lights: Vec<Light>,
+        rising: Vec<Range<f64>>,
+        setting: Vec<Range<f64>>,
+    },
     Any,
 }
 
 impl During {
     pub fn is_any(&self) -> bool {
-        return matches!(self, Self::Any);
+        matches!(self, Self::Any)
     }
 }
 
@@ -65,12 +73,107 @@ impl<'de> Visitor<'de> for DuringVisitor {
         A: serde::de::SeqAccess<'de>,
     {
         let mut vec: Vec<Light> = Vec::new();
+        let mut rising_ranges: Vec<Range<f64>> = Vec::new();
+        let mut setting_ranges: Vec<Range<f64>> = Vec::new();
 
-        while let Some(light) = seq.next_element()? {
-            vec.push(light);
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum F64OrLight {
+            F64(f64),
+            Light(Light),
         }
 
-        Ok(During::Multiple(vec))
+        let mut p1 = None;
+
+        while let Some(entry) = seq.next_element::<F64OrLight>()? {
+            match entry {
+                F64OrLight::Light(light) => vec.push(light),
+                F64OrLight::F64(f) => {
+                    let mut first = match p1.take() {
+                        Some(f) => f,
+                        None => {
+                            p1 = Some(f % 360.0);
+                            continue;
+                        }
+                    };
+
+                    let mut second = f % 360.0;
+
+                    let first_sign = first.signum();
+                    let second_sign = second.signum();
+                    if first_sign != second_sign {
+                        // invalid
+                        continue;
+                    }
+
+                    let rising = first_sign == 1.0;
+                    let push_to = if rising {
+                        &mut rising_ranges
+                    } else {
+                        &mut setting_ranges
+                    };
+
+                    /*
+                    [340, 15] # between rising elevations of -20 degrees to 15 degrees
+                    [-20..0, 0..15]
+                    [-20..15]
+
+                    [-345, -20] # between setting elevations of 15 degrees to -20
+                    [0..15, -20..0]
+                    [-20..15]
+                    */
+
+                    if rising {
+                        if first > 180.0 {
+                            first -= 360.0;
+                        }
+
+                        if second > 180.0 {
+                            second -= 360.0;
+                        }
+                    } else {
+                        if first < -180.0 {
+                            first += 360.0;
+                        }
+
+                        if second < -180.0 {
+                            first += 360.0;
+                        }
+                    }
+
+                    if first > second {
+                        if first != 360_f64 {
+                            push_to.push(0_f64..first);
+                        }
+
+                        if second != 0_f64 {
+                            push_to.push(second..0_f64);
+                        }
+                    } else {
+                        push_to.push(first..second);
+                    }
+                }
+            }
+        }
+
+        let has_lights = !vec.is_empty();
+        let has_elevations = !rising_ranges.is_empty();
+        if has_lights && has_elevations {
+            Ok(During::LightsAndElevation {
+                lights: vec,
+                rising: rising_ranges,
+                setting: setting_ranges,
+            })
+        } else if has_lights {
+            Ok(During::Lights(vec))
+        } else if has_elevations {
+            Ok(During::Elevation {
+                rising: rising_ranges,
+                setting: setting_ranges,
+            })
+        } else {
+            Ok(During::Lights(vec))
+        }
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
@@ -82,6 +185,6 @@ impl<'de> Visitor<'de> for DuringVisitor {
         }
 
         let light = Light::deserialize(StrDeserializer::new(v))?;
-        Ok(During::Single(light))
+        Ok(During::Lights(vec![light]))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,28 @@ async fn main() -> anyhow::Result<()> {
     let now = Utc::now();
     let pos = spa::solar_position::<StdFloatOps>(now, config.location.latitude, config.location.longitude)
         .context("could not determine solar position")?;
-    let light = Light::from(pos);
+    let elevation = 90_f64 - pos.zenith_angle;
+    let light = Light::from(pos.clone());
+    // let declination = calculate_declination(now);
+    // // solar noon is when zenith angle is equal to latitude - solar declination angle
+    // println!("light: {light:?}");
+    // println!("declination: {declination:?}");
+    // println!("azimuth: {}", pos.azimuth);
+    // println!("zenith angle: {}", pos.zenith_angle);
+    // println!("altitude: {altitude}");
+    // println!("solar noon: {}", config.location.latitude - declination.unwrap_or_default());
 
     let wallpaper = config.wallpapers.iter()
         .find(|wp| match &wp.during {
-            During::Single(l) if *l == light => true,
-            During::Multiple(lights) => lights.iter().any(|&l| l == light),
-            _ => false,
+            During::Lights(lights) => lights.iter().any(|&l| l == light),
+            During::Elevation { rising, setting } => (pos.azimuth < 180.0 && rising.iter().any(|range| range.contains(&elevation)))
+                || (pos.azimuth > 180.0 && setting.iter().any(|range| range.contains(&elevation))),
+            During::LightsAndElevation { lights, rising, setting } => {
+                lights.iter().any(|&l| l == light)
+                || (pos.azimuth < 180.0 && rising.iter().any(|range| range.contains(&elevation)))
+                || (pos.azimuth > 180.0 && setting.iter().any(|range| range.contains(&elevation)))
+            }
+            During::Any => false,
         })
         .or_else(|| config.wallpapers.iter().find(|wp| wp.during.is_any()))
         .context("no configured wallpaper")?;
@@ -52,6 +67,76 @@ async fn main() -> anyhow::Result<()> {
         wallpaper.path.to_string_lossy(),
     )).await?;
     Ok(())
+}
+
+// /// Calculates the Sun's declination in degrees.
+// fn calculate_declination(date: DateTime<Utc>) -> Option<f64> {
+//     // -arcsin{0.39779cos[0.98565°(N + 10) + 1.914°sin(0.98565°(N - 2))]}
+//     let first_jan = Utc.with_ymd_and_hms(date.year(), 1, 1, 0, 0, 0).earliest()?;
+//     let n = date.signed_duration_since(first_jan).num_days() as f64;
+//     let a = (1.914_f64).to_radians();
+//     let b = (0.98565_f64).to_radians();
+
+//     let left = b * (n + 10_f64);
+//     let right = a * (b * (n - 2_f64)).sin();
+//     let cos = (left + right).cos();
+//     let result = -(0.39779_f64 * cos).asin();
+
+//     Some(result.to_degrees())
+// }
+
+#[derive(Debug, Clone, Copy)]
+enum LightGeneric {
+    Day,
+    Night,
+    AstronomicalTwilight,
+    NauticalTwilight,
+    CivilTwilight,
+}
+
+impl LightGeneric {
+    const ALL: [LightGeneric; 5] = [
+        Self::Day,
+        Self::Night,
+        Self::AstronomicalTwilight,
+        Self::NauticalTwilight,
+        Self::CivilTwilight,
+    ];
+
+    fn altitude_bounds(self) -> &'static [Range<f64>] {
+        match self {
+            Self::Day => &[
+                -0.25..0.0,
+                0.0..360.00,
+            ],
+            Self::AstronomicalTwilight => &[-18.0..-12.0],
+            Self::NauticalTwilight => &[-12.0..-6.0],
+            Self::CivilTwilight => &[-6.0..-0.25],
+            Self::Night => &[-360.0..-18.0],
+        }
+    }
+
+    fn to_specific(self, azimuth: f64) -> Light {
+        match self {
+            Self::Day => Light::Day,
+            Self::Night => Light::Night,
+            Self::AstronomicalTwilight => if azimuth < 180.0 {
+                Light::AstronomicalDawn
+            } else {
+                Light::AstronomicalDusk
+            },
+            Self::NauticalTwilight => if azimuth < 180.0 {
+                Light::NauticalDawn
+            } else {
+                Light::NauticalDusk
+            },
+            Self::CivilTwilight => if azimuth < 180.0 {
+                Light::CivilDawn
+            } else {
+                Light::CivilDusk
+            },
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Deserialize, PartialEq, Eq)]
@@ -81,55 +166,16 @@ enum Light {
     Night,
 }
 
-fn to_positive_angle(angle: f64) -> f64 {
-    let pos = angle % 360_f64;
-    if pos.signum() == -1.0 {
-        return 360_f64 + pos;
-    }
-
-    pos
-}
-
-impl Light {
-    const ALL: [Light; 8] = [
-        Self::AstronomicalDawn,
-        Self::NauticalDawn,
-        Self::CivilDawn,
-        Self::Day,
-        Self::CivilDusk,
-        Self::NauticalDusk,
-        Self::AstronomicalDusk,
-        Self::Night,
-    ];
-
-    /// Returns the positive solar altitude bounds of this amount of light.
-    fn altitude_bounds(self) -> &'static [Range<f64>] {
-        match self {
-            Self::AstronomicalDawn => &[(360.0 - 18.0)..(360.0 - 12.0)],
-            Self::NauticalDawn => &[(360.0 - 12.0)..(360.0 - 6.0)],
-            Self::CivilDawn => &[(360.0 - 6.0)..(360.0 - 0.25)],
-            Self::Day => &[
-                (360.0 - 0.25)..360.0,
-                0.0..(180.0 + 0.25),
-            ],
-            Self::CivilDusk => &[(180.0 + 0.25)..(180.0 + 6.0)],
-            Self::NauticalDusk => &[(180.0 + 6.0)..(180.0 + 12.0)],
-            Self::AstronomicalDusk => &[(180.0 + 12.0)..(180.0 + 18.0)],
-            Self::Night => &[(180.0 + 18.0)..(360.0 - 18.0)],
-        }
-    }
-}
-
 impl From<SolarPos> for Light {
     fn from(pos: SolarPos) -> Self {
-        let altitude = to_positive_angle(90_f64 - pos.zenith_angle);
-        for light in Light::ALL {
-            if light.altitude_bounds().iter().any(|range| range.contains(&altitude)) {
-                return light;
+        let elevation = 90_f64 - pos.zenith_angle;
+        for light in LightGeneric::ALL {
+            if light.altitude_bounds().iter().any(|range| range.contains(&elevation)) {
+                return light.to_specific(pos.azimuth);
             }
         }
 
-        unreachable!("{altitude}")
+        unreachable!("{elevation}")
     }
 }
 
